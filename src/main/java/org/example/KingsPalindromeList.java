c6038422f619d7227d3013fb1c670c6246e42e61
HBasharan <h.basharan@student.tue.nl>
2026-09-10 11:17:07 +0200

    i deleted it

---
+++ src/main/java/org/example/KingsPalindromeList.java
@@ -0,0 +1,20 @@
+/**
+ * Reads a list of numbers, and can reconstruct the corresponding list of Palindromes,
+ * produce the size of the largest magic set, and the content of that magic set.
+ * 
+ * Usage:
+ * TODO: Documentation
+ * 
+ * END TODO
+ * 
+ * @author <NAME STUDENT 1>
+ * @ID <ID STUDENT 1>
+ * @author <NAME STUDENT 2>
+ * @ID <ID STUDENT 2>
+ * 
+ */
+class KingsPalindromeList {
+    
+    public static void main(String[] args) {
+    }
+}
